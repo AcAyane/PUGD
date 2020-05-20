60ef68225c28927b21791d07104e0e86107edf8b
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-05-20 07:00:54 +0000

    add loanGroup page

---
+++ pages/admin/reporting/customizable/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AdminLayout from '../../../../components/adminLayout'
+const borrowersHome = () => {
+    return <div>
+        this is the reporting customizable
+
+    </div>
+
+}
+borrowersHome.Layout = AdminLayout
+export default borrowersHome
