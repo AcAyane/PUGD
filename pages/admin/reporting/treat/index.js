e05f5188fa8fa9a30fd5cfb978cc5e1449c7cb01
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-05-21 06:25:06 +0000

    add Reservation treat page

---
+++ pages/admin/reporting/treat/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AdminLayout from '../../../../components/adminLayout'
+const CopyHome = () => {
+    return <div>
+        this is the reporting reservation treat
+
+    </div>
+
+}
+CopyHome.Layout = AdminLayout
+export default CopyHome
