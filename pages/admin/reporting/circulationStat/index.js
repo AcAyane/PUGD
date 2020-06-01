97158c41c724493f1c197118503fb1b14fae5789
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-06-01 20:08:57 +0100

    add Periodic Header

--- pages/admin/reporting/circulationStat/index.js
+++ pages/admin/reporting/circulationStat/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import AdminLayout from '../../../../components/adminLayout'
+import ReportingHeader from "../../../../components/admin/reporting/header/reportingHeader"
+import Reporting from "../../../../components/admin/reporting/body/body"
 const CopyHome = () => {
-    return <div>
-
-        this is the reporting circulationStat Page
-
-    </div>
+    return <Reporting>
+        <ReportingHeader ReportingModule="Circulation Stat" />
+    </Reporting>
 
 }
 CopyHome.Layout = AdminLayout
