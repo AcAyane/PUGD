cf6f13ce9484a6e3bb70268d1a3be23fbda052bb
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-06-01 20:19:55 +0100

    add OPAC Header

--- pages/admin/reporting/mailCampaign/index.js
+++ pages/admin/reporting/mailCampaign/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import AdminLayout from '../../../../components/adminLayout'
+import ReportingHeader from "../../../../components/admin/reporting/header/reportingHeader"
+import Reporting from "../../../../components/admin/reporting/body/body"
 const borrowersHome = () => {
-    return <div>
-
-        this is the reporting mailCampaign Page
-
-    </div>
+    return <Reporting>
+        <ReportingHeader ReportingModule="Mail Campaign" />
+    </Reporting>
 
 }
 borrowersHome.Layout = AdminLayout
