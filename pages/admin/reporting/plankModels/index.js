09be31da3613c60f0b271717a09d7ec37e5a6f44
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-06-01 20:13:54 +0100

    add Label Header

--- pages/admin/reporting/plankModels/index.js
+++ pages/admin/reporting/plankModels/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import AdminLayout from '../../../../components/adminLayout'
+import ReportingHeader from "../../../../components/admin/reporting/header/reportingHeader"
+import Reporting from "../../../../components/admin/reporting/body/body"
 const CopyHome = () => {
-    return <div>
-
-        this is the reporting plankModels Page
-
-    </div>
+    return <Reporting>
+        <ReportingHeader ReportingModule="Plank Model" />
+    </Reporting>
 
 }
 CopyHome.Layout = AdminLayout
