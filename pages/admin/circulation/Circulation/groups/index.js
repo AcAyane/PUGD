2c69c90db82f7545a89d9a2eb112170231364a27
Seydina aly <ab.seynali@gmail.com>
2020-06-06 01:46:39 +0100

    Merge branch 'Circulation-FrontEnd-ali'  to https://github.com/Harmony-Technology/PUGD-frontend  'develop'

---
+++ pages/admin/circulation/Circulation/groups/index.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react'
+import AdminLayout from '../../../../../components/adminLayout'
+import Card from '../../../../../components/ui/Card/Card'
+import Grid from '../../../../../components/ui/Grid/grid';
+import GridElement from '../../../../../components/ui/Grid/GridElement';
+import TextBox from "../../../../../components/ui/TextBox";
+import Button from "../../../../../components/ui/Button";
+
+import {useLazyQuery} from "@apollo/react-hooks";
+import {GroupsByName} from "../../../../../graphql/queries/admin/Ciruclation/groups.query";
+import ResGroup from "./resGroups";
+import Null from "../../../../../components/admin/Circulations/Handerls/Null";
+import Circulation from "../../../../../components/admin/Circulations/Body/Body";
+import CirculationHeader from "../../../../../components/admin/Circulations/Hedar/CirculationHeader";
+import Link from "next/link";
+
+const Groups = () => {
+    const [GetGroupsByName, { loading, error, data }] = useLazyQuery(GroupsByName);
+    const [name, setName] = useState('');
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    if(error){
+        console.log(error)
+        console.log(data)
+    }
+
+    const onSearchHandler = (e) => {
+        e.preventDefault();
+
+        GetGroupsByName({
+            variables: {
+                name: name,
+            }
+        });
+    }
+
+    if(data != null){
+        console.log(data.GetGroupsByName)
+    }
+    
+    return <Circulation>
+        <CirculationHeader CirculationModule=" => Group"
+
+                children={
+                    <form>
+                        <div>
+                            <Grid>
+                                <p>Recherche groupe</p>
+                                <GridElement s={12} style={{display:"flex"}}>
+                                    <TextBox
+                                        label="Nom du Group"
+                                        type="text"
+                                        onChange={event => {setName(event.target.value)}}
+                                        value={name}
+                                        />
+                                    <Button
+                                        onClick={onSearchHandler}
+                                        rounded={4}>Search
+                                    </Button>
+                                    <Button href="/admin/circulation/Circulations/groups/addGroups" rounded={4}>add group</Button>
+                                </GridElement>
+                            </Grid>
+                        </div>
+                    </form>
+                }
+        />
+
+
+                {
+
+                    error ? <Null children="No group Finder"/> : (data == null || undefined ) ?
+
+
+                        <ResGroup/> :
+
+                        ('')
+
+                        // : ( <ResGroup datas={data.GetGroupsByName}/> )
+
+                }
+
+    </Circulation>
+}
+Groups.Layout = AdminLayout
+export default Groups
---
+++ pages/admin/circulation/Circulation/pretDoc/index.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+import AdminLayout from '../../../../../components/adminLayout'
+
+import TextBox from "../../../../../components/ui/TextBox";
+import Button from "../../../../../components/ui/Button";
+
+import {useLazyQuery} from "@apollo/react-hooks";
+import {BorrowersByName} from "../../../../../graphql/queries/admin/Ciruclation/Borrowers.query";
+import Circulation from "../../../../../components/admin/Circulations/Body/Body";
+import Null from "../../../../../components/admin/Circulations/Handerls/Null";
+import CirculationHeader from "../../../../../components/admin/Circulations/Hedar/CirculationHeader";
+
+import ListDoc from "./ListDoc";
+
+const pretDoc = () => {
+
+    const [GetBorrowersByName, { loading, error, data }] = useLazyQuery(BorrowersByName);
+
+    const [Name, setName] = useState('');
+
+    if (loading) {return <div>Loading...</div>;}
+
+    if(error){
+        console.log(error)
+        console.log(data)
+    }
+
+    if(data != null || data !== undefined){
+        console.log("donne",data.GetBorrowersByName)
+    }
+    const onSearchHandler = (e) => {
+        e.preventDefault();
+        GetBorrowersByName({
+            variables: {
+                fullname: Name,
+            }
+        });
+    }
+    return <Circulation>
+        <CirculationHeader CirculationModule="Documents prêter"
+                           children={
+                               <from>
+                                   <span>Recherche borrowers</span>
+                                   <div className="row">
+                                       <TextBox label={"Search by Name"}
+                                                type="text"
+                                                onChange={event => {setName(event.target.value)}}
+                                                value={Name}
+                                       />
+                                       <Button
+                                           onClick={onSearchHandler}
+                                           rounded={4}>Search</Button>
+                                   </div>
+                               </from>
+                           }/>
+
+
+        { data != null || data !== undefined ?
+            <ListDoc data = {data.BorrowersByName}/>
+            :
+
+            <Null children="No data" />
+        }
+
+    </Circulation>
+}
+pretDoc.Layout = AdminLayout
+export default pretDoc
