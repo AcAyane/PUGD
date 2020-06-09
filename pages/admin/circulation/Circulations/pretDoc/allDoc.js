12ab8bcc5fccb78c953de49a6bf2b1f0ae3d94bc
majda2019 <48931693+majda2019@users.noreply.github.com>
2020-06-09 02:38:52 +0100

    prêtDoc version1-1

--- pages/admin/circulation/Circulations/pretDoc/allDoc.js
+++ pages/admin/circulation/Circulations/pretDoc/allDoc.js
@@ -1,15 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
 import AdminLayout from '../../../../../components/adminLayout'
-import { useRouter, withRouter } from "next/router";
-import {useQuery, useMutation} from "@apollo/react-hooks";
+import {useRouter} from "next/router";
+import {useQuery} from "@apollo/react-hooks";
 import {GetBorrower} from "../../../../../graphql/queries/admin/Ciruclation/Borrowers.query"
+
 const Alldoc = () => {
 
     const router = useRouter()
     const id = router.query.id;
     console.log(id);
-    const { loading, error, data } = useQuery(GetBorrower, {
-        variables: { id:id },
+    const {loading, error, data} = useQuery(GetBorrower, {
+        variables: {id: id},
     });
     console.log(data)
     return <React.Fragment>
@@ -19,21 +20,32 @@ const Alldoc = () => {
             <div className="col s12 m12 l12">
                 <div className="row vertical-modern-dashboard">
 
-                    <div className="col s12 m8 l8 animate fadeLeft">
+                    <div className="col s12 ">
                         <div className="card">
                             <div className="card-content">
-                                <h4 className="card-title mb-0">Détails Emprunteur:</h4>
-                            <div className="card-content">
-                                <ul>Nom:{data && data.getOneBorrower.last_name }</ul>
-                                <ul>Prénom:{data && data.getOneBorrower.first_name}</ul>
-                                <ul>Email: {data && data.getOneBorrower.email}</ul>
-                                <ul>Adresse:{data && data.getOneBorrower.address.rue1}</ul>
-                                <ul>Contry:{data && data.getOneBorrower.address.contry}</ul>
-                                <ul>Code Barre: {data && data.getOneBorrower.bar_code}</ul>
-                                <ul>Opac: {data && data.getOneBorrower.opaclogin}</ul>
-                                <ul>Sexe: {data && data.getOneBorrower.gender}</ul>
-                                <ul>Date de naissance: {data && data.getOneBorrower.birthday}</ul>
-                            </div>
+                                <h2 className="card-title mb-0">Détails Emprunteur:</h2>
+                                <div className="card-content">
+                                    <div className="row">
+
+                                        <div className="col s12 m4 l2">
+                                            <h5><u>Nom</u>: {data && data.getOneBorrower.last_name}</h5>
+                                            <h5><u>Prénom</u>: {data && data.getOneBorrower.first_name}</h5>
+                                            <h5><u>Email</u>:{data && data.getOneBorrower.email}</h5>
+                                        </div>
+
+                                        <div className="col s12 m4 push-xl2">
+                                            <h5><u>Adresse</u> : {data && data.getOneBorrower.address.rue1}</h5>
+                                            <h5><u>Contry</u> : {data && data.getOneBorrower.address.contry}</h5>
+                                            <h5><u>Code Barre</u> : {data && data.getOneBorrower.bar_code}</h5>
+                                        </div>
+                                        <div className="col s12 m4 push-xl2">
+                                            <h5><u>Opac</u> : {data && data.getOneBorrower.opaclogin}</h5>
+                                            <h5><u>Sexe</u> : {data && data.getOneBorrower.gender}</h5>
+                                            <h5><u>Date de naissance</u> : {data && data.getOneBorrower.birthday}</h5>
+                                        </div>
+
+                                    </div>
+                                </div>
                             </div>
 
                         </div>
@@ -52,7 +64,7 @@ const Alldoc = () => {
                                         <th scope="col">Date</th>
                                         <th scope="col">Retour initial</th>
                                         <th scope="col">Prologement</th>
-                                        <th scope="col">Retour </th>
+                                        <th scope="col">Retour</th>
                                     </tr>
                                     </thead>
                                     <tbody>
