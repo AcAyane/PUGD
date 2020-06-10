0e09f7065fb7e1ca3c101e7570d957d33cd9889e
Mohamed LIDOUH <Mohamed.LIDOUH@telecommancy.eu>
2020-06-10 16:25:29 +0100

    add data into current readers

--- graphql/queries/admin/reporting/borrowers.queries.js
+++ graphql/queries/admin/reporting/borrowers.queries.js
@@ -1,205 +1,26 @@
 import gql from 'graphql-tag';
 
-
-const GET_BORROWERS_CITY  = gql`
-query(
-    $City : String,
-){ GetBorrowersCity(City:$City)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName 
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_BIRTHDAY  = gql`
-query(
-    $Birthday : String,
-){ GetBorrowersCity(Birthday:$Birthday)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_BARCODE  = gql`
-query(
-    $BarCode : String,
-){ GetBorrowersCity(BarCode:$BarCode)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_ADDRESS  = gql`
-query(
-    $Address : String,
-){ GetBorrowersCity(Address:$Address)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName 
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_STATUS  = gql`
-query(
-    $Status : String,
-){ GetBorrowersCity(Status:$Status)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_FIRST_NAME  = gql`
-query(
-    $FirstName : String,
-){ GetBorrowersCity(FistName:$FirstName)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_LAST_NAME  = gql`
-query(
-    $LastName : String,
-){ GetBorrowersCity(LastName:$LastName)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
-const GET_BORROWERS_EMAIL  = gql`
-query(
-    $Email : String,
-){ GetBorrowersCity(Email:$Email)
-  {
-    _id 
-    BarCode
-	FirstName
-	LastName  
-	City                
-	Address             
-	Gender                                                           
-	City                                  
-	Email                    
-	PhoneNumber                   
-	Birthday          
-	Status         
-	MemberShipStart  
-	MemberShipCanceled
-  }
-}
-  
-  `;
-
+const ALL_BORROWERS = gql`
+    query{
+        getAllBorrowers{
+            _id,
+            first_name,
+            last_name,
+        }
+    }
+`;
+const BorrowersByName = gql`
+    query($last: String!){
+       GetBorrowersByName(last : $lastname){
+         _id,
+            firstname,
+            email,
+            gender
+      
+        }
+    }
+`;
 module.exports = {
-    GET_BORROWERS_CITY,
-    GET_BORROWERS_BIRTHDAY,
-    GET_BORROWERS_BARCODE,
-    GET_BORROWERS_ADDRESS,
-    GET_BORROWERS_STATUS,
-    GET_BORROWERS_FIRST_NAME,
-    GET_BORROWERS_LAST_NAME,
-    GET_BORROWERS_EMAIL,
+    ALL_BORROWERS,
+    BorrowersByName
 }
--- pages/admin/reporting/currentReaders/index.js
+++ pages/admin/reporting/currentReaders/index.js
@@ -4,23 +4,50 @@ import ReportingHeader from "../../../../components/admin/reporting/header/repor
 import Reporting from "../../../../components/admin/reporting/body/body"
 import Button from "../../../../components/ui/Button";
 import Table from "../../../../components/ui/Table/Table";
+import {useQuery} from "@apollo/react-hooks";
+import {ALL_BORROWERS} from "../../../../graphql/queries/admin/reporting/borrowers.queries";
 
-const CopyHome = () => {
+const Borrowers = () => {
+    const nul = <span style={{color:'#d60e28'}}>No Group finder</span>;
+    const show = false;
+    const {loading, error, data} = useQuery(ALL_BORROWERS)
+    //console.log("data",data.getAllBorrowers && data.getAllBorrowers.first_name )
+
     return <Reporting>
         <ReportingHeader ReportingModule="Current Readers" />
 
-        <Table
-            Thead={
-                <tr>
-                    <th>BarCode</th>
-                    <th>FullName</th>
-                    <th>Address</th>
-                    <th>City</th>
-                    <th>Birthday</th>
-                    <th>ExpirationDate</th>
-                    <th>Status</th>
-                </tr>
-            }
+        <Table Thead={
+            <tr>
+                <td>Barcode</td>
+                <td>LastName & firstName</td>
+                <td>Address </td>
+                <td>City</td>
+                <td>Birthday</td>
+                <td>DueDate</td>
+                <td>Status</td>
+            </tr>
+        }
+               Tbody={
+
+                   data != null || data !== undefined ?(
+                   data.getAllBorrowers.map((item)=>{
+                   // eslint-disable-next-line react/jsx-key
+                   return <React.Fragment>
+                   <tr>
+                   <td>{item.first_name}</td>
+                   <td>{item.last_name}</td>
+                   <td>{item.first_name}</td>
+                   <td>{item.last_name}</td>
+                   <td>{item.first_name}</td>
+                   <td>{item.last_name}</td>
+                   <td>{item.first_name}</td>
+                   </tr>
+                   </React.Fragment>
+               })
+                       ) : "null"
+
+
+               }
         />
 
         <Button rounded={2}>
@@ -29,5 +56,5 @@ const CopyHome = () => {
     </Reporting>
 
 }
-CopyHome.Layout = AdminLayout
-export default CopyHome
+Borrowers.Layout = AdminLayout
+export default Borrowers
