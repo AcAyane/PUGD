b2c74a82602539c2cbed9616fc6a8dfbf7a06396
kossayrhafiri123 <rkossay@gmail.com>
2020-06-30 12:16:47 -0700

    quick changes

--- pages/admin/cataloguing/Record/AddRecords.js
+++ pages/admin/cataloguing/Record/AddRecords.js
@@ -63,8 +63,8 @@ const AddRecord = () => {
     const [ClassNumber1, setClassNumber1] = useState([])
     const [CollectionTitle, setCollectionTitle] = useState([])
     const [CollectionTitle1, setCollectionTitle1] = useState([])
-    const [Categories, setCategories] = useState([])
-    const [Categories1, setCategories1] = useState([])
+   /* const [Categories, setCategories] = useState([])
+    const [Categories1, setCategories1] = useState([])*/
     const [Author, setAuthor] = useState('')
     const [Function, setFunction] = useState('')
     /* **** */
@@ -109,14 +109,14 @@ const AddRecord = () => {
     c[d] = items.split("\"")[1]
       , d++
     )) 
-     /* **** */
+     /* **** 
      const e = []
      let f = 0
      Categories1.map((items) => (
      e[f] = items.split("\"")[1]
        , f++
      )) 
-     /* ******** */
+      ******** */
      let Responsibilities = []
      let Responsability = new Map()
      Responsability['author'] = Author.split("\"")[1]
@@ -162,12 +162,12 @@ const AddRecord = () => {
             OriginalLanguage : p,
             FkSeries : Serie.split("\"")[1],
             FkSubSeries : SubSerie.split("\"")[1],
-            Publishers : Publishers.split("\"")[1],
-            OtherPublishers:OtherPublishers.split("\"")[1],
+            Publisher : Publishers.split("\"")[1],
+            OtherPublisher:OtherPublishers.split("\"")[1],
             CollectionTitle:c,
             ClassNumber:a,
-            Category:e,
-            Responsability:Responsibilities,
+          /* Responsibility:Responsability,
+              Category:e, */
           }
       });
     }
@@ -209,7 +209,7 @@ const AddRecord = () => {
                    
                   <div className="input-field col s6">
                       
-                      <SelectBox className="validate" required id="uname5" name="uname5" type="text" 
+                      <SelectBox className="validate" required  type="text" 
                       onChange={e => setType(e.target.value)} value={Type}>
                        <option value="a" selected="selected">printed text</option>
                         <option value="b">manuscript text</option>
@@ -223,26 +223,26 @@ const AddRecord = () => {
                         <option value="k">2D graphical document</option>
                         <option value="l">electronic document</option>
                         <option value="m">multimedia document</option>
-                        <option value="r">3D object, artifact, ...</option>
+                        <option value="n">3D object, artifact, ...</option>
                         </SelectBox>
                     </div>
 
                     <div className="input-field col s12" >
-                      <label htmlFor="uname1">Title proper*</label>
-                      <input className="validate" required id="uname1" name="uname1" type="text" 
+                      <label >Title proper*</label>
+                      <input className="validate" required  type="text" 
                       onChange={e => seTitle(e.target.value)} value={title}/>
                     </div>
                    
                     <div className="input-field col s12">
-                      <label htmlFor="uname2">Other title information*</label>
-                      <input className="validate" required id="uname2" name="uname2" type="text" 
+                      <label >Other title information*</label>
+                      <input className="validate" required  type="text" 
                       onChange={e => setOtherTitle(e.target.value)} value={OtherTitle}/>
                     </div>
 
 
                     <div className="input-field col s12">
-                      <label htmlFor="uname3">Parallel title*</label>
-                      <input className="validate" required id="uname3" name="uname3" type="text" 
+                      <label>Parallel title*</label>
+                      <input className="validate" required  type="text" 
                       onChange={e => setParallelTitle(e.target.value)} value={ParallelTitle}/>
                     </div>
                     </div>
@@ -279,7 +279,7 @@ const AddRecord = () => {
               <div className="row" style={{backgroundColor: "#F8F8F8"}}>
               <h6  >Imprint</h6>
               <div className="input-field col s6">
-                        <SelectBox  id="uname14" name="uname14"  className="validate" 
+                        <SelectBox className="validate" 
                         label={"Serie"} onChange={e => setSerie(e.target.value)}
                          required>
                             
@@ -292,7 +292,7 @@ const AddRecord = () => {
                         </SelectBox >
                     </div>
                     <div className="input-field col s6">
-                        <SelectBox  id="uname14" name="uname14"  className="validate" 
+                        <SelectBox  className="validate" 
                         label={"SubSerie"} onChange={e => setSubSerie(e.target.value)}
                          required>
                             
@@ -306,7 +306,7 @@ const AddRecord = () => {
                     </div>
 
                     <div className="input-field col s6">
-                        <SelectBox  id="uname14" name="uname14"  className="validate" 
+                        <SelectBox  className="validate" 
                         label={"Publishers"} onChange={e => setPublishers(e.target.value)}
                          required>
                             
@@ -319,7 +319,7 @@ const AddRecord = () => {
                         </SelectBox >
                     </div>
                     <div className="input-field col s6">
-                        <SelectBox  id="uname14" name="uname14"  className="validate" 
+                        <SelectBox  className="validate" 
                         label={"OtherPublishers"} onChange={e => setOtherPublishers(e.target.value)}
                          required>
                             
@@ -332,8 +332,8 @@ const AddRecord = () => {
                         </SelectBox >
                         </div>
                         <div className="input-field col s6">
-                      <label htmlFor="uname4">Year*</label>
-                      <input type="number" id="quantity" name="quantity" onChange={e => setRecYear(e.target.value)} value={RecYear}/>
+                      <label >Year*</label>
+                      <input type="number" onChange={e => setRecYear(e.target.value)} value={RecYear}/>
                     </div>
                     <div className="input-field col s6">
                       <label htmlFor="uname5">Edition Statement*</label>
@@ -345,8 +345,8 @@ const AddRecord = () => {
                     <div className="row" style={{backgroundColor: "#F8F8F8"}}>
               <h6  >ISBN, EAN, Commercial no.</h6>
               <div className="input-field col s12">
-                      <label htmlFor="uname0">ISBN, EAN, Commercial no.*</label>
-                      <input className="validate" required id="uname0" name="uname0" type="text" 
+                      <label >ISBN, EAN, Commercial no.*</label>
+                      <input className="validate" required type="text" 
                       onChange={e => setIsbn(e.target.value)} value={isbn}/>
                     </div></div>
                    
@@ -354,31 +354,31 @@ const AddRecord = () => {
               <h6  >Collation</h6>
 
               <div className="input-field col s12">
-                      <label htmlFor="uname10">Extent of item (number of pages, elements...)*</label>
-                      <input type="number" id="quantity" name="quantity" onChange={e => setNbPages(e.target.value)} value={NbPages}/>
+                      <label>Extent of item (number of pages, elements...)*</label>
+                      <input type="number" onChange={e => setNbPages(e.target.value)} value={NbPages}/>
                     </div>
 
                     
                     <div className="input-field col s12">
-                      <label htmlFor="uname6">Other properties (illustrations, ...)*</label>
-                      <input className="validate" required id="uname6" name="uname6" type="text" 
+                      <label>Other properties (illustrations, ...)*</label>
+                      <input className="validate" required type="text" 
                       onChange={e => setOtherInformations(e.target.value)} value={OtherInformations}/>
                     </div>
 
                     <div className="input-field col s12">
-                      <label htmlFor="uname7">Format*</label>
-                      <input className="validate" required id="uname7" name="uname7" type="text" 
+                      <label>Format*</label>
+                      <input className="validate" required type="text" 
                       onChange={e => setFormat(e.target.value)} value={Format}/>
                     </div>
 
                     <div className="input-field col s12">
-                      <label htmlFor="uname4">Price*</label>
-                      <input type="number" id="quantity" name="quantity" onChange={e => setPrice(e.target.value)} value={Price}/>
+                      <label >Price*</label>
+                      <input type="number" onChange={e => setPrice(e.target.value)} value={Price}/>
                     </div>
 
                     <div className="input-field col s12">
-                      <label htmlFor="uname9">Accompanying materials*</label>
-                      <input className="validate" required id="uname9" name="uname9" type="text" 
+                      <label >Accompanying materials*</label>
+                      <input className="validate" required type="text" 
                       onChange={e => setAccMaterial(e.target.value)} value={AccMaterial}/>
                     </div> </div>
 
@@ -390,13 +390,13 @@ const AddRecord = () => {
               <h6  >Notes</h6>
                     
               <div className="input-field col s12">
-                      <label htmlFor="uname15">General note*</label>
+                      <label >General note*</label>
                       <textarea  data-length="120" className="materialize-textarea"
                       onChange={e => setNoteAuthor(e.target.value)} value={NoteAuthor}/>
                     </div> 
 
                     <div className="input-field col s12">
-                      <label htmlFor="uname8">Summary*</label>
+                      <label >Summary*</label>
                       <textarea  data-length="120" className="materialize-textarea"
                       onChange={e => setSummary(e.target.value)} value={Summary}/>
                     </div> </div>
@@ -405,10 +405,10 @@ const AddRecord = () => {
                     <div className="row" style={{backgroundColor: "#F8F8F8"}}>
               <h6  >Indexing</h6>
               <div className="input-field col s6">
-                        <SelectBox  id="uname13" name="uname13" className="validate" label={"KeyWords"}
+                        <SelectBox className="validate" label={"KeyWords"}
                          setInstance={setKeyWords} multiple onChange={e => setKeyWords1(KeyWords.getSelectedValues())} required>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         { data2.keywords.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.Word} </option>
@@ -420,10 +420,10 @@ const AddRecord = () => {
 
 
                     <div className="input-field col s6">
-                        <SelectBox   multiple className="validate" setInstance={setCategories}
-                         onChange={e => setCategories1(Categories.getSelectedValues())} label={"Categories"}>
+                        <SelectBox   multiple className="validate" /*setInstance={setCategories}
+                         onChange={e => setCategories1(Categories.getSelectedValues())} */ label={"Categories"}>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         { data9.category_authority.map((items) => (
 
                         <option key={items._id}  value={items._id}> {items.name} </option>
@@ -435,11 +435,11 @@ const AddRecord = () => {
 
 
                     <div className="input-field col s6">
-                        <SelectBox  id="uname14" multiple name="uname14" setInstance={setBranches} className="validate" 
+                        <SelectBox   multiple setInstance={setBranches} className="validate" 
                         label={"Branches"} onChange={e => setBranches1(Branches.getSelectedValues())}
                          required>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         { data3.branches.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.BranchName} </option>
@@ -454,11 +454,11 @@ const AddRecord = () => {
                  
 
                     <div className="input-field col s6">
-                        <SelectBox  id="uname14" multiple name="uname14" setInstance={setClassNumber} className="validate" 
+                        <SelectBox  multiple setInstance={setClassNumber} className="validate" 
                         label={"Class Number"} onChange={e => setClassNumber1(ClassNumber.getSelectedValues())}
                          required>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         { data8.class_number.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.name} </option>
@@ -471,11 +471,11 @@ const AddRecord = () => {
                     <div className="row" style={{backgroundColor: "#F8F8F8"}}>
               <h6  >Uniform Titles</h6>
                     <div className="input-field col s12">
-                        <SelectBox  id="uname14" multiple name="uname14" setInstance={setCollectionTitle} className="validate" 
+                        <SelectBox multiple  setInstance={setCollectionTitle} className="validate" 
                         label={"Uniform Titles"} onChange={e => setCollectionTitle1(CollectionTitle.getSelectedValues())}
                          required>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         { data7.collection_title.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.title} </option>
@@ -488,10 +488,10 @@ const AddRecord = () => {
                     <div className="row" style={{backgroundColor: "#F8F8F8"}}>
                     <h6  >Language of publication</h6>
                     <div className="input-field col s6">
-                        <SelectBox  id="uname11" multiple="multiple" name="uname11" label={"Language of publication"} className="validate" 
+                        <SelectBox   multiple="multiple"  label={"Language of publication"} className="validate" 
                          setInstance={setLanguage} onChange={e => setLanguage1(Language.getSelectedValues())}>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         {data1.languages.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.Value} </option>
@@ -501,10 +501,10 @@ const AddRecord = () => {
                     </div>
 
                     <div className="input-field col s6">
-                        <SelectBox  id="uname12" name="uname12" multiple="multiple" className="validate" label={"Original Language"}
+                        <SelectBox   multiple="multiple" className="validate" label={"Original Language"}
                         setInstance={setOriginalLanguage} onChange={e => setOriginalLanguage1(OriginalLanguage.getSelectedValues())} required>
                             
-                        <option value selected disabled >Choose your option</option>
+                        <option value disabled >Choose your option</option>
                         {data1.languages.map((items) => (
 
                             <option key={items._id}  value={items._id}> {items.Value} </option>
